6c5e6c802df39c4a7309cf27e10b34df6fe3fe06
Vedran Abramovic <vedran.abramovic@ag04.com>
2018-05-08 22:22:31 +0200

    Add Javadoc and refactor code

--- src/main/java/com/brama/vogue/service/impl/ScoreServiceImpl.java
+++ src/main/java/com/brama/vogue/service/impl/ScoreServiceImpl.java
@@ -10,13 +10,30 @@ import com.brama.vogue.consumer.WebServiceConsumer;
 import com.brama.vogue.entity.Score;
 import com.brama.vogue.service.ScoreService;
 
+/**
+ * The Class ScoreServiceImpl is a service which utilizes web service consumer
+ * and applies logic of how should score be calculated .
+ */
 @Service
 public class ScoreServiceImpl implements ScoreService {
+
+	/** The Constant logger. */
 	private static final Logger logger = LoggerFactory.getLogger(ScoreServiceImpl.class);
 
+	/** The web service consumer. */
 	@Autowired
 	WebServiceConsumer webServiceConsumer;
 
+	/**
+	 * Calculates popularity of a term in GitHub issues on scale from 1 to 10 based
+	 * on how many times it was mentioned in issues as "some_term rocks" or as
+	 * "some_term sucks"
+	 * 
+	 * @param String
+	 *            term
+	 * 
+	 * @return Score score
+	 */
 	@Cacheable(value = "scores", key = "#term")
 	public Score getScore(String term) {
 		logger.info("Cache MISS for key '{}'", term);
